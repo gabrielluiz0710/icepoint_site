b21b0eb971873464b26fa465827127a949758434
Gabriel Luiz <gabriel.luiz@ufu.br>
2024-09-01 02:04:39 -0300

    Pelo amor de deus dá certo a home inteira

---
+++ src/layout/Cards/Slider.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination, A11y } from 'swiper/modules';
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import './Slider.css'
+
+interface SliderProps {
+    settings: SwiperProps;
+    children: ReactNode;
+}
+
+const Slider: React.FC<SliderProps> = ({ settings, children }) => {
+    return (
+        <div className="content">
+            <Swiper modules={[Navigation, Pagination, A11y]}{...settings}>
+            {children}
+        </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
