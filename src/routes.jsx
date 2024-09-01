c12c0017a1bb45fccec302c30ecf74fe1f66b6b7
Gabriel Luiz <gabriel.luiz@ufu.br>
2024-09-01 02:58:17 -0300

    Fix

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home'
-import Localização from './pages/Localização'
-import Carrinho from './pages/Carrinho'
+import Localização from './pages/localização'
+import Carrinho from './pages/carrinho'
 
 
 function AppRoutes(){
