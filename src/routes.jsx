2006b564b1377c4c01f1ed75cf6b1cdfafe8ec41
Gabriel Luiz <gabriel.luiz@ufu.br>
2024-09-01 02:59:51 -0300

    Teste

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home'
-import Localização from './pages/localização'
+import Localização from './pages/localizacao'
 import Carrinho from './pages/carrinho'
 
 
